ebeabba41f28499e116bb9fcf1ff23357523ca3c
毕克有 <596737739@qq.com>
2023-03-29 10:41:53 +0800

    feat: 设置插件配置

--- vite.config.js
+++ vite.config.js
@@ -18,5 +18,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  chainWebpack: config => {
+    // config.plugins.delete('prefetch')
   }
 })
