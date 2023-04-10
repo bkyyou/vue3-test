8ab0e2cbac0af8709e1c9323eec27d587474de4c
毕克有 <596737739@qq.com>
2023-04-10 13:51:31 +0800

    feat: 新增下载文件

---
+++ src/views/download/Index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import {
+  reactive,
+  ref,
+  onMounted,
+  onUpdated,
+  watch,
+  watchEffect,
+  computed,
+  defineProps,
+  provide,
+} from 'vue';
+import { download } from '@/api/index.js';
+
+
+
+</script>
+
+<template>
+  <div>
+    <div>
+      <button @click="download">下载</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
