d8fda87d0b83e1510c6362b1f547d66f46c7baf7
毕克有 <596737739@qq.com>
2023-04-10 13:50:06 +0800

    feat: 新增下载文件

---
+++ src/api/index.js
@@ -0,0 +1,41 @@
+import $http from "../utils/http";
+import { saveAs } from 'file-saver';
+
+export const  download = async () => {
+  return new Promise(reslove => {
+    $http.post('/downloadExecl', {}, {responseType: 'blob'}).then(res => {
+      console.log(res);
+      /**
+       * 下载 二
+       */
+      saveAs(res.data, 'test.xlsx')
+      /**
+       * 下载 一
+       */
+      // if (window.navigator.msSaveBlob) {
+      //   // 兼容 ie type 太长了 ie 想看 ie 里打印看一下，没ie浏览器
+      //   window.navigator.msSaveBlob(res.data, 
+      //     {type: ''},
+      //     'test.xlsx',
+      //   )
+      // } else {
+      //   const blobUrl = window.URL.createObjectURL(res.data);
+      //   console.log(blobUrl);
+      //   const link = document.createElement('a');
+      //   link.href = blobUrl;
+      //   link.download = 'test.xlsx';
+      //   link.style.display = 'none';
+      //   link.click();
+      //   window.URL.revokeObjectURL(blobUrl);
+      //   // link.remove();
+      // }
+
+      /**
+       * 预览
+       */
+      // url -》 线上地址
+      // base64URL blob -> 对应的预览处 -> fileReader
+      reslove();
+    })
+  })
+}
