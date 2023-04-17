7015b2e0d380e4be4105da73bcf458d1e87b0b44
毕克有 <596737739@qq.com>
2023-04-17 16:23:20 +0800

    feat: ts 测试, md5 加密 和 路由跳转

--- src/views/globalApiTest/Test1.vue
+++ src/views/globalApiTest/Test1.vue
@@ -1,3 +1,4 @@
+<!-- <script setup lang="ts"> -->
 <script setup>
 import {
   reactive,
@@ -10,13 +11,21 @@ import {
   defineProps,
   defineEmits,
   provide,
+  // Ref,
 } from 'vue';
+import {useRouter} from 'vue-router';
+import { getData } from '@/api/index.js';
+import md5 from 'js-md5';
+const router = useRouter();
 
+// const num1: Ref<number> = ref(0)
 const num1 = ref(0)
 
 const add = () => {
   num1.value = num1.value + 1;
 }
+
+console.log(md5('1111111') === '7fa8282ad93047a4d6fe6111c93b308a');
 </script>
 
 <style scoped>
@@ -26,4 +35,7 @@ const add = () => {
 <template>
     <div>test1: {{ num1 }}</div>
     <div @click="add">add</div>
+    <div @click="() => {
+      router.push('/')
+    }">跳转1111</div>
 </template>
